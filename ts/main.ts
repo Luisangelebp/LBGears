b1bc9b0f36d01722a1c5ada377a5ebbf5ebe4955
Luisangelebp <luisangelebp55@gmail.com>
2022-09-02 20:44:02 -0400

    main

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,3 @@
 import mobil from './includes/mobil-menu';
 const d = document;
-d.getElementById('mobil-des')!.addEventListener('click', mobil)
+d.querySelector('#mobil-des div')!.addEventListener('click', mobil)
